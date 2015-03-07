fafd03966c90ce228c17a312f9ab31b0642e4afb
May-Li Khoe <mayli@khanacademy.org>
2015-03-07 01:47:00 -0500

    some reusable arrays of KA-based early math colors + 7 shades of grey

---
+++ kacolors.js
@@ -0,0 +1,24 @@
+
+// these are the early math riffs off of the KA color palette *for now*
+// it would be nice to have these pulled off of something central eventually!
+
+var kacolors = [];
+
+kacolors[0] = new Color({hue: 0.78, saturation: 0.41, brightness: 0.61})
+kacolors[1] = new Color({hue: 0.96, saturation: 0.60, brightness: 0.71})
+kacolors[2] = new Color({hue: 0.01, saturation: 0.66, brightness: 0.98})
+kacolors[3] = new Color({hue: 0.08, saturation: 0.67, brightness: 0.91})
+kacolors[4] = new Color({hue: 0.28, saturation: 0.55, brightness: 0.72})
+kacolors[5] = new Color({hue: 0.45, saturation: 0.60, brightness: 0.78})
+kacolors[6] = new Color({hue: 0.53, saturation: 0.67, brightness: 0.79})
+kacolors[7] = new Color({hue: 0.57, saturation: 0.41, brightness: 0.58})
+
+var kagreys = []
+
+kagreys[0] = new Color({hue: 0.0, saturation: 0.0, brightness: 0.07})
+kagreys[1] = new Color({hue: 0.0, saturation: 0.0, brightness: 0.20})
+kagreys[2] = new Color({hue: 0.0, saturation: 0.0, brightness: 0.33})
+kagreys[3] = new Color({hue: 0.0, saturation: 0.0, brightness: 0.60})
+kagreys[4] = new Color({hue: 0.0, saturation: 0.0, brightness: 0.67})
+kagreys[5] = new Color({hue: 0.0, saturation: 0.0, brightness: 0.87})
+kagreys[6] = new Color({hue: 0.0, saturation: 0.0, brightness: 0.93})
