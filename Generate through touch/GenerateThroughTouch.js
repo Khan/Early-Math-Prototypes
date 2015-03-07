60bf03a7b2b9eb926c0f46dd5cf9fa97cf70a494
May-Li Khoe <mayli@khanacademy.org>
2015-03-07 01:46:29 -0500

    The dots count themselves, and fly around, and bounce off walls

--- Generate through touch/GenerateThroughTouch.js
+++ Generate through touch/GenerateThroughTouch.js
@@ -3,22 +3,23 @@
 /* 
 
 Generate lots of things, see the count go up
-	still riffing off of Counting by touch count!
-	built on Prototope @cf00c1f
 
 Future direction:
 	Things that fly! 
 	Maybe you can pop things or chase them off the screen and see the number subtract!
 
+Bugs:
+	For some reason you can't catch + throw the dots
+
 */
 
 Layer.root.backgroundColor = new Color({hue: 0.0, saturation: 0.0, brightness: 1.0})
 
-// TODO: you currently can't attach touch handlers to the root layer. oops.
+// make a screen sized layer to catch all the touches in lieu of root catching all touches for now
 var touchCatchingLayer = new Layer()
 touchCatchingLayer.frame = Layer.root.bounds
 
-var touchLayers = [];
+// ka color set up
 var kacolors = [];
 
 kacolors[0] = new Color({hue: 0.78, saturation: 0.41, brightness: 0.61})
@@ -30,11 +31,32 @@ kacolors[5] = new Color({hue: 0.45, saturation: 0.60, brightness: 0.78})
 kacolors[6] = new Color({hue: 0.53, saturation: 0.67, brightness: 0.79})
 kacolors[7] = new Color({hue: 0.57, saturation: 0.41, brightness: 0.58})
 
+var kagreys = []
+
+kagreys[0] = new Color({hue: 0.0, saturation: 0.0, brightness: 0.07})
+kagreys[1] = new Color({hue: 0.0, saturation: 0.0, brightness: 0.20})
+kagreys[2] = new Color({hue: 0.0, saturation: 0.0, brightness: 0.33})
+kagreys[3] = new Color({hue: 0.0, saturation: 0.0, brightness: 0.60})
+kagreys[4] = new Color({hue: 0.0, saturation: 0.0, brightness: 0.67})
+kagreys[5] = new Color({hue: 0.0, saturation: 0.0, brightness: 0.87})
+kagreys[6] = new Color({hue: 0.0, saturation: 0.0, brightness: 0.93})
+
+// array to keep the generated layers
+var touchLayers = [];
+var touchLayerVelocity = [];
+var countFromLayer = {}
+
+// some variables to regulate behavior
+var frictionConstant = 0.999
+var minMaxInitialVelocity = 5
+
+var numberLayer = new TextLayer()
+makeCountLayer();
+
+// make circles when you touch anywhere
 
 var z = 0
 touchCatchingLayer.touchBeganHandler = function(touchSequence) {
-
-	console.log(touchSequence.currentSample.globalLocation.x)
 	
 	makeCircleLayer(touchSequence.currentSample.globalLocation, touchSequence.currentSample.globalLocation.zPosition)
 
@@ -42,12 +64,26 @@ touchCatchingLayer.touchBeganHandler = function(touchSequence) {
 }
 
 touchCatchingLayer.touchEndedHandler = touchCatchingLayer.touchCancelledHandler = function(touchSequence) {
-
+	//really the thing shouldn't start to animate perhaps until after your finger has lifted?
 }
+
+var causeFriction = new ActionBehavior({handler:function(layer) {
+		for (var i = 0; i < touchLayers.length; i++) {
+
+			var newVelocityWithFriction = touchLayerVelocity[i].multiply(frictionConstant)
+			//hee hee, this is doing strange things. i really need to check for a *direction change*
+			 
+			touchLayerVelocity[i] = newVelocityWithFriction
+		}
+
+	}
+})
+
+touchCatchingLayer.behaviors = [causeFriction] 
 
 function makeCircleLayer(position, zposition) {
 
-	var touchCircleLayer = new Layer()
+	var touchCircleLayer = new Layer({name:(touchLayers.length.toString())})
 	touchCircleLayer.position = position
 	touchCircleLayer.width = touchCircleLayer.height = 75
 	touchCircleLayer.cornerRadius = touchCircleLayer.width / 2.0
@@ -56,4 +92,83 @@ function makeCircleLayer(position, zposition) {
 	touchCircleLayer.backgroundColor = kacolors[randomIndex]
 	touchCircleLayer.zPosition = zposition
 
+	var randomX = Math.random()
+	var randomY = Math.random()
+	var randomXVelocity = map({value: randomX, fromInterval: [0,1], toInterval: [-minMaxInitialVelocity, minMaxInitialVelocity]})
+	var randomYVelocity = map({value: randomY, fromInterval: [0,1], toInterval: [-minMaxInitialVelocity, minMaxInitialVelocity]})
+	var randomVelocity = new Point({x: randomXVelocity, y: randomYVelocity})
+
+	touchLayers.push(touchCircleLayer)
+	updateCountLayer()
+	touchLayerVelocity.push (randomVelocity)
+	countFromLayer[touchCircleLayer.name] = touchLayers.length - 1
+
+	var drift = new ActionBehavior({handler:function(layer) {
+		var layerIndex = countFromLayer[layer.name]
+		var newPosition = layer.position.add(touchLayerVelocity[layerIndex]) //this is a really dumb long way around
+		layer.position = newPosition
+		if ((newPosition.x + layer.width/2 > Layer.root.width)||(newPosition.x - layer.width/2 < 0)) {
+			var tempPoint = new Point({x: (0 - touchLayerVelocity[layerIndex].x), y: touchLayerVelocity[layerIndex].y})
+			touchLayerVelocity[layerIndex] = tempPoint
+		}
+		if ((newPosition.y + layer.height/2 > Layer.root.height)||(newPosition.y - layer.height/2 < 0)) {
+			var tempPoint = new Point({x: touchLayerVelocity[layerIndex].x, y: (0 - touchLayerVelocity[layerIndex].y)})
+			touchLayerVelocity[layerIndex] = tempPoint
+		}
+		//the stuff for checking how things bump into each other, which I have not done a good job of below
+		//however now I am yak shaving. NEXT PROTO
+		// for (var i = 0; i < touchLayers.length; i++) {
+		// 	if (i != layerIndex) {
+		// 		var absPositionDiffX = Math.abs((touchLayers[i].position.subtract(newPosition).x))
+		// 		var absPositionDiffY = Math.abs((touchLayers[i].position.subtract(newPosition).y))
+		// 		if ((absPositionDiffX < layer.width)&&(absPositionDiffY < layer.width)) {s
+		// 			var tempPoint = new Point({x: (0 - touchLayerVelocity[layerIndex].x), y: (0 - touchLayerVelocity[layerIndex].y)})
+		// 			touchLayerVelocity[layerIndex] = tempPoint
+					
+		// 		}
+		// 	}
+		// }	
+
+
+		//console.log(layerIndex)
+	}})
+
+	touchCircleLayer.behaviors = [drift];
+
+	touchCircleLayer.touchBeganHandler = function(touchSequence) {
+		touchCircleLayer.behaviors = [];
+	}
+
+	touchCircleLayer.touchMovedHandler = function(touchSequence) {
+		touchCircleLayer.position = touchCircleLayer.position.add(touchSequence.currentSample.globalLocation.subtract(touchSequence.previousSample.globalLocation))
+	}
+
+	touchCircleLayer.touchEndedHandler = function(touchSequence) {
+		var newVelocity = touchSequence.currentSample.globalLocation.subtract(touchSequence.previousSample.globalLocation)
+		var layerIndex = countFromLayer[touchCircleLayer.name]
+		touchLayerVelocity[layerIndex] = newVelocity
+		touchCircleLayer.behaviors = [drift]; 
+	}
 }
+
+
+function makeCountLayer() {
+ 
+	numberLayer.textColor = kagreys[2]
+	numberLayer.fontName = "Avenir-Heavy"
+	numberLayer.fontSize = 45
+	numberLayer.text = touchLayers.length.toString()
+	numberLayer.zPosition = 100000
+	numberLayer.x = Layer.root.x
+	numberLayer.y = 125
+
+	return numberLayer
+}
+
+function updateCountLayer() {
+	numberLayer.text = (touchLayers.length - 1).toString() + " + 1 = " + touchLayers.length.toString()
+	numberLayer.x = Layer.root.x
+}
+
+
+
