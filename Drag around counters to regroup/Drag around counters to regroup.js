018939d752064b41821a97864363c36c684fe267
Andy Matuschak <andy@khanacademy.org>
2015-03-10 17:15:43 -0400

    New target visuals

--- Drag around counters to regroup/Drag around counters to regroup.js
+++ Drag around counters to regroup/Drag around counters to regroup.js
@@ -18,11 +18,46 @@ if (Layer.root.width !== 1024) {
 	throw "This prototype wants to be run in landscape on an iPad!"
 }
 
-var counterDiameter = 44
-var leftSideColor = new Color({hue: 0.95, saturation: 1.0, brightness: 0.95})
-var rightSideColor = new Color({hue: 0.55, saturation: 1.0, brightness: 0.95})
+//============================================================================================
 
-Layer.root.backgroundColor = new Color({hue: 0.7, saturation: 0.12, brightness: 1.0})
+// these are the early math riffs off of the KA color palette *for now*
+// it would be nice to have these pulled off of something central eventually!
+
+var kacolors = [];
+
+kacolors[0] = new Color({hue: 0.78, saturation: 0.41, brightness: 0.61})
+kacolors[1] = new Color({hue: 0.96, saturation: 0.60, brightness: 0.71})
+kacolors[2] = new Color({hue: 0.01, saturation: 0.66, brightness: 0.98})
+kacolors[3] = new Color({hue: 0.08, saturation: 0.67, brightness: 0.91})
+kacolors[4] = new Color({hue: 0.28, saturation: 0.55, brightness: 0.72})
+kacolors[5] = new Color({hue: 0.45, saturation: 0.60, brightness: 0.78})
+kacolors[6] = new Color({hue: 0.53, saturation: 0.67, brightness: 0.79})
+kacolors[7] = new Color({hue: 0.57, saturation: 0.41, brightness: 0.58})
+
+var kagreys = []
+
+kagreys[0] = new Color({hue: 0.0, saturation: 0.0, brightness: 0.07})
+kagreys[1] = new Color({hue: 0.0, saturation: 0.0, brightness: 0.20})
+kagreys[2] = new Color({hue: 0.0, saturation: 0.0, brightness: 0.33})
+kagreys[3] = new Color({hue: 0.0, saturation: 0.0, brightness: 0.60})
+kagreys[4] = new Color({hue: 0.0, saturation: 0.0, brightness: 0.67})
+kagreys[5] = new Color({hue: 0.0, saturation: 0.0, brightness: 0.87})
+kagreys[6] = new Color({hue: 0.0, saturation: 0.0, brightness: 0.93})
+
+//============================================================================================
+
+// Game state:
+var leftTargetCount = 5
+var rightTargetCount = 3
+var leftInitialCount = 2
+var rightInitialCount = 6
+var hasWon = false
+
+var counterDiameter = 44
+var leftSideColor = kacolors[2]
+var rightSideColor = kacolors[6]
+
+Layer.root.backgroundColor = Color.white
 
 var label1 = makeCurrentCountLabel()
 var label2 = makeCurrentCountLabel()
@@ -31,25 +66,23 @@ label1.originY = label2.originY = 30
 label1.x = Layer.root.x / 2
 label2.x = Layer.root.x * 3/2
 
-var target1 = 5
-var target2 = 3
-var hasWon = false
 
 // Generate the counters
 var sidePaddingX = 50 + counterDiameter / 2
-var sidePaddingY = 125 + counterDiameter / 2
-populateSide(new Rect({x: sidePaddingX, y: sidePaddingY, width: Layer.root.width / 2 - sidePaddingX * 2, height: Layer.root.height - sidePaddingY * 2}), 2, leftSideColor)
-populateSide(new Rect({x: Layer.root.width / 2 + sidePaddingX, y: sidePaddingY, width: Layer.root.width / 2 - sidePaddingX * 2, height: Layer.root.height - sidePaddingY * 2}), 6, rightSideColor)
+var sidePaddingTopY = 150 + counterDiameter / 2
+var sidePaddingBottomY = 250 + counterDiameter / 2
+populateSide(new Rect({x: sidePaddingX, y: sidePaddingTopY, width: Layer.root.width / 2 - sidePaddingX * 2, height: Layer.root.height - sidePaddingTopY - sidePaddingBottomY}), 2, leftSideColor)
+populateSide(new Rect({x: Layer.root.width / 2 + sidePaddingX, y: sidePaddingTopY, width: Layer.root.width / 2 - sidePaddingX * 2, height: Layer.root.height - sidePaddingTopY - sidePaddingBottomY}), 6, rightSideColor)
 
-label1.text = "2"
+label1.text = leftInitialCount.toString()
 label1.textColor = leftSideColor
 
-label2.text = "6"
+label2.text = rightInitialCount.toString()
 label2.textColor = rightSideColor
 
 // Draw a line in the middle
 var centerLine = new Layer()
-centerLine.backgroundColor = Color.white
+centerLine.backgroundColor = kagreys[5]
 centerLine.frame = Layer.root.bounds
 centerLine.width = 1
 
@@ -88,10 +121,11 @@ function populateSide(frame, counterCount, counterColor) {
 function makeCurrentCountLabel() {
 	var label = new TextLayer()
 	label.fontName = "Futura"
-	label.fontSize = 60
+	label.fontSize = 70
 	label.textColor = Color.white
 	label.text = "0"
 	label.zPosition = 10000
+	label.shadow = new Shadow({color: Color.white, radius: 0, alpha: 1.0, offset: new Size({width: 1, height: 1})})
 	return label
 }
 
@@ -100,7 +134,7 @@ function makeCounter() {
 
 	var randomHue = Math.random()
 	counter.width = counter.height = counterDiameter
-	counter.cornerRadius = counter.width / 2.0
+	counter.cornerRadius = 15
 	counter.animators.scale.springSpeed = 30
 	counter.animators.backgroundColor.springSpeed = 30
 	counter.animators.backgroundColor.springBounciness = 0
@@ -124,7 +158,7 @@ function makeCounter() {
 			counter.animators.backgroundColor.target = leftSideColor
 		}
 
-		if (parseInt(label1.text) == target1 && parseInt(label2.text) == target2 && !hasWon) {
+		if (parseInt(label1.text) == leftTargetCount && parseInt(label2.text) == rightTargetCount && !hasWon) {
 			hasWon = true
 			Layer.root.userInteractionEnabled = false
 			showYay()
@@ -139,6 +173,73 @@ function makeCounter() {
 	return counter
 }
 
+var leftThoughtBubble = makeThoughtBubble("Red", leftTargetCount)
+var rightThoughtBubble = makeThoughtBubble("Blue", rightTargetCount)
+leftThoughtBubble.y = rightThoughtBubble.y = Layer.root.frameMaxY - leftThoughtBubble.height / 2.0 - 30
+leftThoughtBubble.x = label1.x + 90
+rightThoughtBubble.x = label2.x - 90
+
+var leftThoughtBubbleAux1 = makeFloatingCircle(30)
+var leftThoughtBubbleAux2 = makeFloatingCircle(20)
+var leftThoughtBubbleAux3 = makeFloatingCircle(10)
+leftThoughtBubbleAux1.backgroundColor = leftThoughtBubbleAux2.backgroundColor = leftThoughtBubbleAux3.backgroundColor = leftSideColor
+leftThoughtBubbleAux1.position = leftThoughtBubble.origin.add(new Point({x: -10, y: 20}))
+leftThoughtBubbleAux2.position = leftThoughtBubble.origin.add(new Point({x: -40, y: 10}))
+leftThoughtBubbleAux3.position = leftThoughtBubble.origin.add(new Point({x: -60, y: 25}))
+
+var rightThoughtBubbleAux1 = makeFloatingCircle(30)
+var rightThoughtBubbleAux2 = makeFloatingCircle(20)
+var rightThoughtBubbleAux3 = makeFloatingCircle(10)
+rightThoughtBubbleAux1.backgroundColor = rightThoughtBubbleAux2.backgroundColor = rightThoughtBubbleAux3.backgroundColor = rightSideColor
+var rightSideOfRightThoughtBubble = new Point({x: rightThoughtBubble.frameMaxX, y: rightThoughtBubble.originY})
+rightThoughtBubbleAux1.position = rightSideOfRightThoughtBubble.add(new Point({x: 0, y: 20}))
+rightThoughtBubbleAux2.position = rightSideOfRightThoughtBubble.add(new Point({x: 35, y: 10}))
+rightThoughtBubbleAux3.position = rightSideOfRightThoughtBubble.add(new Point({x: 60, y: 25}))
+
+var leftThoughtBubbleLabel = makeThoughtBubbleLabel(leftTargetCount)
+
+function makeThoughtBubble(imageNameExtension, value) {
+	var thoughtBubble = new Layer({imageName: "Cloud-" + imageNameExtension})
+	thoughtBubble.position = Layer.root.position
+
+	thoughtBubble.behaviors = [makeFloatingBehavior(0.040, 0.5)]
+
+	var thoughtBubbleLabel = new TextLayer({parent: thoughtBubble})
+	thoughtBubbleLabel.textColor = Color.white
+	thoughtBubbleLabel.fontName = "Futura"
+	thoughtBubbleLabel.fontSize = 60
+	thoughtBubbleLabel.text = value.toString()
+	thoughtBubbleLabel.x = thoughtBubble.width / 2.0
+	thoughtBubbleLabel.y = thoughtBubble.height / 2.0
+
+	return thoughtBubble
+}
+
+function makeFloatingCircle(diameter) {
+	var circle = new Layer()
+	circle.width = circle.height = diameter
+	circle.cornerRadius = diameter / 2.0
+	circle.behaviors = [makeFloatingBehavior(0.035, 0.40)]
+	return circle
+}
+
+function makeThoughtBubbleLabel(value) {
+}
+
+function makeFloatingBehavior(amplitude, frequency) {
+	var floatingSeed = Math.random() * 2 * Math.PI
+	return new ActionBehavior({handler: function(layer) {
+		layer.y = layer.y + Math.sin((Timestamp.currentTimestamp() + floatingSeed) * frequency) * amplitude
+	}})
+}
+
+var leftPlaceholderGuy = new Layer({imageName: "placeholder guy"})
+var rightPlaceholderGuy = new Layer({imageName: "placeholder guy"})
+leftPlaceholderGuy.y = rightPlaceholderGuy.y = Layer.root.frameMaxY - leftPlaceholderGuy.height / 2.0 + 35
+leftPlaceholderGuy.originX = 80
+rightPlaceholderGuy.originX = Layer.root.frameMaxX - rightPlaceholderGuy.width - 80
+rightPlaceholderGuy.scaleX = -1
+
 var CounterSide = {
 	left: 0,
 	right: 1
@@ -151,7 +252,7 @@ function showYay() {
 	var yay = new TextLayer()
 	yay.fontName = "Futura"
 	yay.fontSize = 500
-	yay.textColor = new Color({hue: 0.2, saturation: 0.2, brightness: 1.0})
+	yay.textColor = kacolors[5]
 	yay.text = "YAY"
 	yay.scale = 0.01
 	yay.position = Layer.root.position
