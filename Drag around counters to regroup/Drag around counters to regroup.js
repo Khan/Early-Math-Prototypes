f42866a89a0b46474ecb1737b385102543b62701
Andy Matuschak <andy@khanacademy.org>
2015-03-10 15:36:19 -0400

    Regrouping prototype now has two sides instead of circles, no target card. WIP.

--- Drag around counters to regroup/Drag around counters to regroup.js
+++ Drag around counters to regroup/Drag around counters to regroup.js
@@ -3,7 +3,10 @@
 Drag to match cards
 	built on Prototope @ 7093cfe206
 
-Here we're building 1-to-1 relationships between the counters and the numerals, and reinforcing the idea of conservation: by moving a token from one circle to the other, we lower the number on the right side by 1 and increase the number on the left side by 1.
+Here we're building 1-to-1 relationships between the counters and the
+numerals, and reinforcing the idea of conservation: by moving a token from one
+circle to the other, we lower the number on the right side by 1 and increase
+the number on the left side by 1.
 
 Key weaknesses here:
 	the prompt isn't really inherently fun / creative / discovery-ish. 
@@ -15,38 +18,44 @@ if (Layer.root.width !== 1024) {
 	throw "This prototype wants to be run in landscape on an iPad!"
 }
 
+var counterDiameter = 44
+
 Layer.root.backgroundColor = new Color({hue: 0.7, saturation: 0.2, brightness: 1.0})
 
-var circle1 = makeCircle()
-var label1 = makeCircleLabel()
-var circle2 = makeCircle()
-var label2 = makeCircleLabel()
+var label1 = makeCurrentCountLabel()
+var label2 = makeCurrentCountLabel()
 
-circle1.y = circle2.y = Layer.root.y * 0.7
-circle1.originX = (Layer.root.x - 50) - circle1.width
-circle2.originX = Layer.root.x + 50
-
-label1.x = circle1.x
-label2.x = circle2.x
-label1.originY = label2.originY = circle1.originY - label1.height - 15
+label1.originY = label2.originY = 40
+label1.x = Layer.root.x / 2
+label2.x = Layer.root.x * 3/2
 
 var target1 = 5
 var target2 = 3
 var hasWon = false
 
-populateCircle(circle1, 2)
-label1.text = "2"
+// Generate the counters
+var sidePaddingX = 50 + counterDiameter / 2
+var sidePaddingY = 100 + counterDiameter / 2
+populateSide(new Rect({x: sidePaddingX, y: sidePaddingY, width: Layer.root.width / 2 - sidePaddingX * 2, height: Layer.root.height - sidePaddingY * 2}), 2)
+populateSide(new Rect({x: Layer.root.width / 2 + sidePaddingX, y: sidePaddingY, width: Layer.root.width / 2 - sidePaddingX * 2, height: Layer.root.height - sidePaddingY * 2}), 6)
 
-populateCircle(circle2, 6)
+label1.text = "2"
 label2.text = "6"
 
-var targetCard = new Layer()
-targetCard.backgroundColor = Color.white
-targetCard.x = Layer.root.x
-targetCard.y = Layer.root.frameMaxY - 100
-targetCard.width = 700
-targetCard.height = 350
-targetCard.cornerRadius = 60
+// Draw a line in the middle
+var centerLine = new Layer()
+centerLine.backgroundColor = Color.white
+centerLine.frame = Layer.root.bounds
+centerLine.width = 1
+
+// var targetCard = new Layer()
+// targetCard.backgroundColor = Color.white
+// targetCard.x = Layer.root.x
+// targetCard.y = Layer.root.frameMaxY - 100
+// targetCard.width = 700
+// targetCard.height = 350
+// targetCard.cornerRadius = 60
+// targetCard.alpha = 0
 
 function makeTargetCircle() {
 	var circle = new Layer()
@@ -67,41 +76,39 @@ function makeTargetLabel(targetValue, position) {
 	label.userInteractionEnabled = false
 }
 
-var targetCircle1 = makeTargetCircle()
-targetCircle1.x = targetCard.x - targetCircle1.width / 2.0 - 25
-targetCircle1.touchBeganHandler = function() {
-	circle1.animators.scale.target = new Point({x: 1.15, y: 1.15})
-}
-targetCircle1.touchEndedHandler = targetCircle1.touchCancelledHandler = function() {
-	circle1.animators.scale.target = new Point({x: 1, y: 1})
-}
-makeTargetLabel(target1, targetCircle1.position)
+// var targetCircle1 = makeTargetCircle()
+// targetCircle1.x = targetCard.x - targetCircle1.width / 2.0 - 25
+// targetCircle1.touchBeganHandler = function() {
+// 	circle1.animators.scale.target = new Point({x: 1.15, y: 1.15})
+// }
+// targetCircle1.touchEndedHandler = targetCircle1.touchCancelledHandler = function() {
+// 	circle1.animators.scale.target = new Point({x: 1, y: 1})
+// }
+// makeTargetLabel(target1, targetCircle1.position)
 
-var targetCircle2 = makeTargetCircle()
-targetCircle2.x = targetCard.x + targetCircle2.width / 2.0 + 25
-targetCircle2.touchBeganHandler = function() {
-	circle2.animators.scale.target = new Point({x: 1.15, y: 1.15})
-}
-targetCircle2.touchEndedHandler = targetCircle1.touchCancelledHandler = function() {
-	circle2.animators.scale.target = new Point({x: 1, y: 1})
-}
-makeTargetLabel(target2, targetCircle2.position)
+// var targetCircle2 = makeTargetCircle()
+// targetCircle2.x = targetCard.x + targetCircle2.width / 2.0 + 25
+// targetCircle2.touchBeganHandler = function() {
+// 	circle2.animators.scale.target = new Point({x: 1.15, y: 1.15})
+// }
+// targetCircle2.touchEndedHandler = targetCircle1.touchCancelledHandler = function() {
+// 	circle2.animators.scale.target = new Point({x: 1, y: 1})
+// }
+// makeTargetLabel(target2, targetCircle2.position)
 
-function populateCircle(circle, counterCount) {
+function populateSide(frame, counterCount) {
 	var counters = []
 	for (var i = 0; i < counterCount; i++) {
 		var counter = makeCounter()
 
 		while (true) {
+			// Put it somewhere in the side.
 			counter.position = new Point({
-				x: Math.floor(Math.random() * (circle.frameMaxX - circle.originX) + circle.originX),
-				y: Math.floor(Math.random() * (circle.frameMaxY - circle.originY) + circle.originY)
+				x: Math.floor(Math.random() * (frame.maxX - frame.minX) + frame.minX),
+				y: Math.floor(Math.random() * (frame.maxY - frame.minY) + frame.minY)
 			})
 
-			if (!circleContainsPoint(circle.position, (circle.width - counter.width) / 2.0, counter.position)) {
-				continue;
-			}
-
+			// Try again if it hit a counter we've already put down.
 			var hitOtherCounter = false
 			for (var otherCounterIndex in counters) {
 				var otherCounter = counters[otherCounterIndex]
@@ -120,20 +127,13 @@ function populateCircle(circle, counterCount) {
 	}
 }
 
-function makeCircle() {
-	var circle = new Layer()
-	circle.border = new Border({color: Color.white, width: 4})
-	circle.width = circle.height = 300
-	circle.cornerRadius = circle.width / 2.0
-	return circle
-}
-
-function makeCircleLabel() {
+function makeCurrentCountLabel() {
 	var label = new TextLayer()
 	label.fontName = "Futura"
 	label.fontSize = 60
 	label.textColor = Color.white
 	label.text = "0"
+	label.zPosition = 10000
 	return label
 }
 
@@ -142,8 +142,7 @@ function makeCounter() {
 
 	var randomHue = Math.random()
 	counter.backgroundColor = new Color({hue: randomHue, saturation: 0.4, brightness: 1.0})
-	//counter.border = new Border({color: new Color({hue: randomHue, saturation: 0.4, brightness: 0.9}), width: 2})
-	counter.width = counter.height = 44
+	counter.width = counter.height = counterDiameter
 	counter.cornerRadius = counter.width / 2.0
 	counter.animators.scale.springSpeed = 30
 
@@ -151,82 +150,26 @@ function makeCounter() {
 		counter.animators.scale.target = new Point({x: 1.5, y: 1.5})
 	}
 	counter.touchMovedHandler = function(touchSequence) {
-		var bounceVelocity = -800
-		if (circleLayerContainsPoint(circle1, touchSequence.previousSample.globalLocation) &&
-			!circleLayerContainsPoint(circle1, touchSequence.currentSample.globalLocation)) {
-			var oldCircle1Value = parseInt(label1.text)
-			targetCircle1.animators.y.target = targetCircle1.y
-			targetCircle1.animators.x.target = targetCircle1.x
-			if (oldCircle1Value > target1) {
-				targetCircle1.animators.y.velocity = bounceVelocity
-				targetCircle1.animators.x.velocity = 0
-			} else {
-				targetCircle1.animators.x.velocity = bounceVelocity
-				targetCircle1.animators.y.velocity = 0
-			}
-			label1.text = (oldCircle1Value - 1).toString()
-		} else if (!circleLayerContainsPoint(circle1, touchSequence.previousSample.globalLocation) &&
-			circleLayerContainsPoint(circle1, touchSequence.currentSample.globalLocation)) {
-			var oldCircle1Value = parseInt(label1.text)
-			targetCircle1.animators.y.target = targetCircle1.y
-			targetCircle1.animators.x.target = targetCircle1.x
-			if (oldCircle1Value < target1) {
-				targetCircle1.animators.y.velocity = bounceVelocity
-				targetCircle1.animators.x.velocity = 0
-			} else {
-				targetCircle1.animators.x.velocity = bounceVelocity
-				targetCircle1.animators.y.velocity = 0
-			}
-			label1.text = (oldCircle1Value + 1).toString()
-		}
+		var previousSide = sideForCounterPosition(touchSequence.previousSample.globalLocation)
+		var currentSide = sideForCounterPosition(touchSequence.currentSample.globalLocation)
 
-		// lol copy pasta too tired to think about abstraction
-		if (circleLayerContainsPoint(circle2, touchSequence.previousSample.globalLocation) &&
-			!circleLayerContainsPoint(circle2, touchSequence.currentSample.globalLocation)) {
-			var oldCircle2Value = parseInt(label2.text)
-			targetCircle2.animators.y.target = targetCircle2.y
-			targetCircle2.animators.x.target = targetCircle2.x
-			if (oldCircle2Value > target2) {
-				targetCircle2.animators.y.velocity = bounceVelocity
-				targetCircle2.animators.x.velocity = 0
-			} else {
-				targetCircle2.animators.x.velocity = bounceVelocity
-				targetCircle2.animators.y.velocity = 0
-			}
-			label2.text = (oldCircle2Value - 1).toString()
-		} else if (!circleLayerContainsPoint(circle2, touchSequence.previousSample.globalLocation) &&
-			circleLayerContainsPoint(circle2, touchSequence.currentSample.globalLocation)) {
-			var oldCircle2Value = parseInt(label2.text)
-			targetCircle2.animators.y.target = targetCircle2.y
-			targetCircle2.animators.x.target = targetCircle2.x
-			if (oldCircle2Value < target2) {
-				targetCircle2.animators.y.velocity = bounceVelocity
-				targetCircle2.animators.x.velocity = 0
-			} else {
-				targetCircle2.animators.x.velocity = bounceVelocity
-				targetCircle2.animators.y.velocity = 0
-			}
-			label2.text = (oldCircle2Value + 1).toString()
+		var previousSide1Value = parseInt(label1.text)
+		var previousSide2Value = parseInt(label2.text)
+		if (previousSide === CounterSide.left && currentSide === CounterSide.right) {
+			label1.text = (previousSide1Value - 1).toString()
+			label2.text = (previousSide2Value + 1).toString()
+		} else if (previousSide === CounterSide.right && currentSide === CounterSide.left) {
+			label1.text = (previousSide1Value + 1).toString()
+			label2.text = (previousSide2Value - 1).toString()
 		}
 
 		if (parseInt(label1.text) == target1 && parseInt(label2.text) == target2 && !hasWon) {
 			hasWon = true
-
 			Layer.root.userInteractionEnabled = false
-			var yay = new TextLayer()
-			yay.fontName = "Futura"
-			yay.fontSize = 500
-			yay.textColor = new Color({hue: 0.2, saturation: 0.2, brightness: 1.0})
-			yay.text = "YAY"
-			yay.scale = 0.01
-			yay.position = Layer.root.position
-			yay.animators.scale.target = new Point({x: 1, y: 1})
+			showYay()
 		}
 
-		var newPosition = counter.position.add(touchSequence.currentSample.globalLocation.subtract(touchSequence.previousSample.globalLocation))
-		if (!targetCard.containsGlobalPoint(newPosition)) {
-			counter.position = newPosition
-		}
+		counter.position = counter.position.add(touchSequence.currentSample.globalLocation.subtract(touchSequence.previousSample.globalLocation))
 	}
 	counter.touchEndedHandler = counter.touchCancelledHandler = function(touchSequence) {
 		counter.animators.scale.target = new Point({x: 1.0, y: 1.0})
@@ -235,10 +178,21 @@ function makeCounter() {
 	return counter
 }
 
-function circleLayerContainsPoint(circle, point) {
-	return point.distanceToPoint(circle.position) < (circle.width / 2.0)
+var CounterSide = {
+	left: 0,
+	right: 1
+}
+function sideForCounterPosition(position) {
+	return (position.x < Layer.root.x) ? CounterSide.left : CounterSide.right
 }
 
-function circleContainsPoint(position, radius, point) {
-	return point.distanceToPoint(position) < radius
+function showYay() {
+	var yay = new TextLayer()
+	yay.fontName = "Futura"
+	yay.fontSize = 500
+	yay.textColor = new Color({hue: 0.2, saturation: 0.2, brightness: 1.0})
+	yay.text = "YAY"
+	yay.scale = 0.01
+	yay.position = Layer.root.position
+	yay.animators.scale.target = new Point({x: 1, y: 1})
 }
