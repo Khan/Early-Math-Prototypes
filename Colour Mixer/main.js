8aaffa600cf6c4e8a7c050e7aeabefabc4bed32c
Jason Brennan <i.jasonbrennan@gmail.com>
2015-05-22 14:18:45 -0400

    om nom nom

--- Colour Mixer/main.js
+++ Colour Mixer/main.js
@@ -7,32 +7,53 @@ var color = {
 }
 
 
+var colorConstants = {
+	red: new Color({hex: "e65b4c"}),
+	green: Color.green,
+	blue: Color.blue
+}
+
+
 mixer.updateBackgroundColor = function() {
-	log(color)
 	mixer.backgroundColor = new Color({red: color.red, green: color.green, blue: color.blue})
 }
 
-log(color["red"])
+
 function makeSquare(bgColor, colorName) {
 	var layer = new Layer()
 	
 	var size = 150
 	layer.size = new Size({width: size, height: size})
-	layer.backgroundColor = bgColor
+	layer.backgroundColor = new Color({hex: "EEEEEE"})
 	layer.cornerRadius = 8
+
+	var innerLayer = new Layer({parent: layer})
+	innerLayer.backgroundColor = bgColor
+	innerLayer.size = new Size({width: 50, height: 50})
+	innerLayer.cornerRadius = 4
+	innerLayer.moveToCenterOfParentLayer()
 	
 	layer.gestures = [new TapGesture({handler: function() {
+		layer.addMoreColor()
+	}})]
+
+
+	layer.addMoreColor = function() {		
 		color[colorName] += 0.1
 		mixer.updateBackgroundColor()
-	}})]
+		innerLayer.animators.scale.target = new Point({x: 1, y: 1})
+		innerLayer.animators.scale.velocity = new Point({x: 4, y: 4})
+	}
+
+	layer.innerLayer = innerLayer
 	
 	return layer
 }
 
 // Using RGB for now because modelling RYB on a computer seemed like a rabit hole not worth going down quite yet...
-var red = makeSquare(Color.red, "red")
-var blue = makeSquare(Color.blue, "blue")
-var green = makeSquare(Color.green, "green")
+var red = makeSquare(colorConstants.red, "red")
+var blue = makeSquare(colorConstants.blue, "blue")
+var green = makeSquare(colorConstants.green, "green")
 
 green.moveToHorizontalCenterOfParentLayer()
 red.moveToLeftOfSiblingLayer({siblingLayer: green, margin: 100})
@@ -48,8 +69,103 @@ mixer.cornerRadius = 8
 mixer.moveToCenterOfParentLayer()
 mixer.updateBackgroundColor()
 
+var blockSize = 25
+var redBrick = makeBrick({length: 3, color: colorConstants.red, target: red})
+var greenBrick = makeBrick({length: 3, color: colorConstants.green, target: green})
+var blueBrick = makeBrick({length: 3, color: colorConstants.blue, target: blue})
 
+//-------------------------------------------------
+// Bricks
+//-------------------------------------------------
+
+function makeEmptyBlock(args) {
+	var color = args.color
+
+	var rect = new Rect({x: 50, y: 50, width: blockSize, height: blockSize})
+	var block = new Layer()
+	block.frame = rect
+	block.cornerRadius = 8
+
+	block.backgroundColor = color
+
+
+	return block
+}
+
+function makeBrick(args) {
+	var length = args.length
+
+	if (length < 1) { return }
+
+	var container = new Layer()
+	container.size = new Size({width: (blockSize + 2) * length, height: blockSize})
+	container.blocks = []
+
+	var maxX = 0
+	for (var index = 0; index < length; index++) {
+		var color = args.color
+		var block = makeEmptyBlock({color: color})
+		
+		block.parent = container
+		block.originX = maxX + 2
+		block.originY = 0
+
+		maxX = block.frameMaxX
+
+		container.blocks.push(block)
+	}
+
+
+	container.showIfNeeded = function() {
+		if (container.alpha == 1) { return }
+
+		container.alpha = 1
+		container.scale = 0.01
+		container.animators.scale.target = new Point({x: 1, y: 1})
+	}
+
+
+	var target = args.target
+	container.becomeDraggable = function() {
+
+		var initialPositionInContainer = new Point()
+		container.touchBeganHandler = function(touchSequence) {
+			initialPositionInContainer = touchSequence.currentSample.locationInLayer(container)
+		}
+	
+		container.touchMovedHandler = function(touchSequence) {
+			var position = touchSequence.currentSample.globalLocation
+			container.origin = position.subtract(initialPositionInContainer)
+		}
+
+		container.touchEndedHandler = function(touchSequence) {
+			if (target.frame.contains(touchSequence.currentSample.globalLocation)) {
+				target.addMoreColor()
+				container.animators.scale.target = new Point({x: 0.01, y: 0.01})
+				container.animators.scale.velocity = new Point({x: 5, y: 5})
+				container.animators.scale.springBounciness = 2
+
+				container.animators.position.target = target.convertLocalPointToGlobalPoint(target.innerLayer.position)
+			}
+		}
+	}
+
+	container.becomeDraggable()
+	return container
+}
+
+
+//-----------------------------------------
+// Helpers
+//-----------------------------------------
 function log(obj) {
 	console.log(JSON.stringify(obj, null, 4))
-	
+}
+
+
+// Hack to bring a layer to the front...this should be a part of prototope!
+function bringLayerToFront(layer) {
+	var parent = layer.parent
+	layer.parent = undefined
+	layer.parent = parent
 }
