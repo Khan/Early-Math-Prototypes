335d7b52460fe02453f3a29ecdf8caad6e5ca4fb
Andy Matuschak <andy@khanacademy.org>
2015-05-22 12:05:05 -0700

    Added comments to ghost blocks proto

--- Subtraction - ghost blocks in nature/Subtraction - ghost blocks in nature.js
+++ Subtraction - ghost blocks in nature/Subtraction - ghost blocks in nature.js
@@ -2,31 +2,34 @@ if (Layer.root.width !== 1024) {
 	throw "This prototype is meant to be run in landscape on an iPad"
 }
 
+// where the ghost blocks start
 const ghostContainerX = 610
-const blockContainerX = 346
 const ghostContainerY = 578
-const stairContainerSlotCount = 6
+
+// where the non-ghost blocks are
+const blockContainerX = 346
+const blockContainerY = 280
+
+const totalGhostBlockCount = 6
+
 const blockWidth = 42
 const lineWidth = 1
 const ghostStrokeColor = new Color({white: 0.8})
 
 const bg = new Layer({imageName: "bg"})
 bg.userInteractionEnabled = false
-bg.moveToBottomSideOfParentLayer()
-bg.originX = 0
+bg.position = Layer.root.position
 
-for (let blockIndex = 0; blockIndex < stairContainerSlotCount; blockIndex++) {
-	var block = makeBlock(6 - blockIndex)
+// Make the ghost blocks.
+for (let blockIndex = 0; blockIndex < totalGhostBlockCount; blockIndex++) {
+	var block = makeGhostBlock(6 - blockIndex)
 	block.originX = ghostContainerX + blockIndex * (blockWidth + 6)
 	block.originY = ghostContainerY - block.height
 }
 
 let highestBlockZPosition = 0
 
-let slotBlocks = []
-slotBlocks.fill(null, 0, stairContainerSlotCount)
-
-function makeBlock(count) {
+function makeGhostBlock(count) {
 	const blockContainer = new Layer()
 	blockContainer.width = blockWidth
 	blockContainer.height = (blockWidth + lineWidth) * count
@@ -40,6 +43,7 @@ function makeBlock(count) {
 	block.strokeWidth = 3
 	block.fillColor = Color.white
 
+	// Draw lines separating each block
 	for (var blockIndex = 0; blockIndex < count; blockIndex++) {
 		if (blockIndex > 0) {
 			const y = blockIndex * (blockWidth + lineWidth)
@@ -62,11 +66,6 @@ function makeBlock(count) {
 	blockContainer.touchBeganHandler = () => {
 		highestBlockZPosition += 1
 		blockContainer.zPosition = highestBlockZPosition
-
-		var currentSlot = slotBlocks.indexOf(blockContainer)
-		if (currentSlot !== undefined) {
-			slotBlocks[currentSlot] = undefined
-		}
 	}
 
 	blockContainer.touchMovedHandler = touchSequence => {
@@ -77,16 +76,15 @@ function makeBlock(count) {
 		blockContainer.animators.scale.target = new Point({x: 1, y: 1})
 		const slot = stairContainerSlotForBlockContainerFrame(blockContainer.frame)
 		if (slot !== undefined) {
+			// Snap to a block
 			const position = new Point({
 				x: xPositionForSlotIndex(slot) + blockWidth / 2.0 + (slot + 1) * lineWidth,
-				y: 280 + clip({
-					value: Math.round((blockContainer.originY - 280) / (blockWidth + lineWidth)),
+				y: blockContainerY + clip({
+					value: Math.round((blockContainer.originY - blockContainerY) / (blockWidth + lineWidth)),
 					min: 0, max: Number.MAX_VALUE
 				}) * (blockWidth + lineWidth) + blockContainer.height / 2.0
 			})
 			blockContainer.animators.position.target = position
-
-			slotBlocks[slot] = blockContainer
 		}
 	}
 
@@ -95,7 +93,7 @@ function makeBlock(count) {
 
 function stairContainerSlotForBlockContainerFrame(frame) {
 	var slotIndex = Math.round((frame.minX - blockContainerX) / blockWidth)
-	if (slotIndex < 0 || slotIndex >= stairContainerSlotCount) {
+	if (slotIndex < 0 || slotIndex >= totalGhostBlockCount) {
 		return undefined
 	} else {
 		return slotIndex
