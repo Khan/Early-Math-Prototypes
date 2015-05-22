dd97e835068835bab0a53967eee67590e5455aee
Andy Matuschak <andy@khanacademy.org>
2015-05-22 12:09:53 -0700

    Add comments to the polygon proto

--- Add side to polygon/Add side to polygon.js
+++ Add side to polygon/Add side to polygon.js
@@ -11,18 +11,25 @@ polygon.strokeColor = Color.orange
 polygon.fillColor = new Color({red: 1, green: 0.5, blue: 0, alpha: 0.2})
 polygon.segments = polygonTargetSegments(false)
 
+// We make two handles (the "dots" that appear when you drag the line near it)
+// for the polygon. 0 and 3 here refer to the two segment indices which
+// overlap when the polygon's a triangle (see polygonTargetSegments())
 polygon.leftHandle = makeHandle(polygon)
 polygon.leftHandle.position = polygon.segments[0].point
 polygon.rightHandle = makeHandle(polygon)
 polygon.rightHandle.position = polygon.segments[3].point
 
+// The polygon's continuously animating its handles and segments to some target.
 polygon.behaviors = [
 	new ActionBehavior({handler: () => {
 		if (polygon.targetSegments !== undefined) {
 			let segments = new Array(4)
 			for (let segmentIndex = 0; segmentIndex < 4; segmentIndex++) {
-				segments[segmentIndex] = new Segment(polygon.segments[segmentIndex].point.add(polygon.targetSegments[segmentIndex].point.subtract(polygon.segments[segmentIndex].point).multiply(0.2)))
+				// Exponential decay:
+				const delta = polygon.targetSegments[segmentIndex].point.subtract(polygon.segments[segmentIndex].point)
+				segments[segmentIndex] = new Segment(polygon.segments[segmentIndex].point.add(delta.multiply(0.2)))
 			}
+			// Copy pasta on the exponential decay here; being lazy:
 			polygon.leftHandle.position = polygon.leftHandle.position.add(polygon.targetSegments[0].point.subtract(polygon.segments[0].point).multiply(0.2))
 			polygon.rightHandle.position = polygon.rightHandle.position.add(polygon.targetSegments[3].point.subtract(polygon.segments[3].point).multiply(0.2))
 			polygon.segments = segments
@@ -30,6 +37,7 @@ polygon.behaviors = [
 	}})
 ]
 
+// This is the extra edge you can drag onto the polygon:
 let edge = new ShapeLayer()
 edge.segments = [
 	new Segment(new Point({x: 0, y: shapeSize / 2.0})),
@@ -68,6 +76,7 @@ Layer.root.touchBeganHandler = sequence => {
 Layer.root.touchMovedHandler = sequence => {
 	hitLayer.position = hitLayer.position.add(sequence.currentSample.globalLocation.subtract(sequence.previousSample.globalLocation))
 	if (polygon.caughtEdge && hitLayer === polygon) {
+		// The edge moves along with the polygon when it's "caught" on the polygon.
 		edge.position = edge.position.add(sequence.currentSample.globalLocation.subtract(sequence.previousSample.globalLocation))	
 	}
 
@@ -84,6 +93,7 @@ Layer.root.touchMovedHandler = sequence => {
 Layer.root.touchEndedHandler = sequence => {
 	if (polygonIsOpenForEdgePosition(edge.position)) {
 		edge.animators.position.target = polygon.position.add(new Point({x: 0, y: shapeSize / 2.0}))
+		// Make the edge move with the polygon until it's "detached":
 		polygon.caughtEdge = true
 	} else {
 		polygon.caughtEdge = false
@@ -101,6 +111,7 @@ function polygonIsOpenForEdgePosition(position) {
 	return (position.distanceToPoint(polygon.position) < shapeSize / 1.5)
 }
 
+// The polygon's "open" when `edge` will form the bottom side.
 function polygonTargetSegments(isOpen) {
 	if (isOpen) {
 		return [
@@ -119,6 +130,7 @@ function polygonTargetSegments(isOpen) {
 	}
 }
 
+// Makes the little circular handle thing on the vertices.
 function makeHandle(parent) {
 	const handle = new ShapeLayer.Circle({parent: parent, center: Point.zero, radius: 10})
 	handle.strokeColor = undefined
