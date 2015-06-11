cea837ddf384c45f71c80ebd7330010da1ac1ac7
May-Li Khoe <mayli@khanacademy.org>
2015-06-11 04:28:50 -0700

    Quick test of parallax on touch (not yet animated)

--- Basic mechanix/Basic Mechanix.js
+++ Basic mechanix/Basic Mechanix.js
@@ -19,16 +19,39 @@ const numCharLayers = 1;
 
 Layer.root.backgroundColor = new Color({hue: 0.52, saturation: 0.17, brightness: 0.94})
 
+
+
 //set up BG layers
+var bgParentLayer = new Layer({name:"bgParent"}) 
 for (let i = numFirstBGLayer; i < numFirstBGLayer+numBGLayers; i++) {
 	const bgLayerName = strBgLayer + i
-	var bgLayer = new Layer({imageName: bgLayerName})
+	var bgLayer = new Layer({parent: bgParentLayer, imageName: bgLayerName})
 	bgLayer.x = bgLayer.width/2 
 	bgLayer.y = bgLayer.height/2
 }
 
-//set up character
+const touchCatchingLayer = new Layer()
+touchCatchingLayer.frame = Layer.root.bounds
+
+//set up initial character layer (no static animation yet. eventually: at least blinking!)
 const charLayerName = strCharLayer + numFirstCharLayer
-var charLayer = new Layer({imageName: charLayerName})
-charLayer.x = 170
-charLayer.y = 555
+var charParentLayer = new Layer({name:"charParent"}) 
+var charLayer = new Layer({parent: charParentLayer, imageName: charLayerName})
+charParentLayer.x = 170
+charParentLayer.y = 555
+
+//setting up a basic tap to move character test with basic touch handlers
+touchCatchingLayer.touchBeganHandler = function(touchSequence) {
+ 
+	let i = 0;
+	for (let layerIndex in bgParentLayer.sublayers) {
+		const layer = bgParentLayer.sublayers[layerIndex]
+		layer.x = layer.x - (50*(i+1)); 
+		i++ 
+	}
+
+
+}
+
+
+
