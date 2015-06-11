3452dc5c6fdfe11a2b8bd23d9b551e81cb449e78
May-Li Khoe <mayli@khanacademy.org>
2015-06-11 03:35:44 -0700

    Added placeholder character asset
    Prepping it character for a looped animation and giving them a starting
    position

--- Basic mechanix/Basic Mechanix.js
+++ Basic mechanix/Basic Mechanix.js
@@ -12,23 +12,23 @@ We'll just focus on the forest for now.
 //setting up some strings to point to assets so that they are all in one place
 const strBgLayer = "trees"
 const numFirstBGLayer = 0
-const numLayers = 4
+const numBGLayers = 4
+const strCharLayer = "placeholderkid"
+const numFirstCharLayer = 0
+const numCharLayers = 1;
 
 Layer.root.backgroundColor = new Color({hue: 0.52, saturation: 0.17, brightness: 0.94})
 
-for (let i = numFirstBGLayer; i < numFirstBGLayer+numLayers; i++) {
+//set up BG layers
+for (let i = numFirstBGLayer; i < numFirstBGLayer+numBGLayers; i++) {
 	const bgLayerName = strBgLayer + i
 	var bgLayer = new Layer({imageName: bgLayerName})
 	bgLayer.x = bgLayer.width/2 
 	bgLayer.y = bgLayer.height/2
 }
 
-// const bgLayer0 = new Layer({imageName: strBgLayer0})
-// const bgLayer1 = new Layer({imageName: strBgLayer1})
-// bgLayer0.x = bgLayer0.width/2 
-// bgLayer0.y = bgLayer0.height/2
-// bgLayer1.x = bgLayer1.width/2 
-// bgLayer1.y = bgLayer1.height/2
-
-
- 
+//set up character
+const charLayerName = strCharLayer + numFirstCharLayer
+var charLayer = new Layer({imageName: charLayerName})
+charLayer.x = 170
+charLayer.y = 555
